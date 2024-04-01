f0af144d339d661a5f7523aa3508541cda932f9a
Nemanja Mitic <mitic.nemanja@hotmail.com>
2024-04-01 19:18:23 +0200

    refactor: Some functionality and removed unnecessary code

--- src/hooks/useFormattedData.ts
+++ src/hooks/useFormattedData.ts
@@ -1,35 +1,33 @@
-import { useState, useEffect } from 'react';
-import { SortingFunction, Predicate, User } from '../types';
+import { useMemo, useState } from 'react';
+import { Predicate, SortingFunction, User } from '../types';
 
 export const useFormattedData = (initialData: User[]) => {
-  const [data, _] = useState<User[]>(initialData);
   const [formattedData, setFormattedData] = useState<User[]>(initialData);
 
   const search = (searchTerm: string) => {
-    const filteredData = data.filter((user) =>
+    const filteredData = formattedData.filter((user) =>
       Object.values(user).some((value) => value.toString().toLowerCase().includes(searchTerm.toLowerCase()))
     );
     setFormattedData(filteredData);
   };
 
-  const filter = (predicate: Predicate<User>) => {
-    const filteredData = data.filter(predicate);
-    setFormattedData(filteredData);
-  };
+    const filter = useMemo(() => {
+      const filterFunction = (predicate: Predicate<User>) => {
+        const filteredData = initialData.filter(predicate);
+        setFormattedData(filteredData);
+      };
+      return filterFunction;
+    }, [initialData]);
 
-const sortBy = (filterCriteria: string | SortingFunction<User>) => {
+const sortBy = (filterCriteria: keyof User | SortingFunction<User>) => {
   let sortedData;
   if (typeof filterCriteria === 'string') {
-    sortedData = [...formattedData].sort((a, b) => (a[filterCriteria as keyof User] as string).localeCompare(b[filterCriteria as keyof User] as string));
+    sortedData = [...formattedData].sort((a, b) => (a[filterCriteria] as string).localeCompare(b[filterCriteria] as string));
   } else {
     sortedData = [...formattedData].sort(filterCriteria);
   }
   setFormattedData(sortedData);
 };
-
-  useEffect(() => {
-    setFormattedData(data);
-  }, [data]);
 
   return { formatted: formattedData, search, filter, sortBy };
 };
