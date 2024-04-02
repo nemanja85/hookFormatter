78e5e012b336116d7b7f788a33df80dc6a91803d
Nemanja Mitic <mitic.nemanja@hotmail.com>
2024-04-02 12:33:47 +0200

    fix: Issue releted to set state in App file

--- src/hooks/useFormattedData.ts
+++ src/hooks/useFormattedData.ts
@@ -1,19 +1,19 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Predicate, SortingFunction, User } from '../types';
 
 export const useFormattedData = (initialData: User[]) => {
   const [formattedData, setFormattedData] = useState<User[]>(initialData);
 
   const search = (searchTerm: string) => {
-    const filteredData = formattedData.filter((user) =>
+    const searchedData = formattedData.filter((user) =>
       Object.values(user).some((value) => value.toString().toLowerCase().includes(searchTerm.toLowerCase()))
     );
-    setFormattedData(filteredData);
+    setFormattedData(searchedData);
   };
 
     const filter = useMemo(() => {
       const filterFunction = (predicate: Predicate<User>) => {
-        const filteredData = initialData.filter(predicate);
+        const filteredData = formattedData.filter(predicate);
         setFormattedData(filteredData);
       };
       return filterFunction;
@@ -26,8 +26,13 @@ const sortBy = (filterCriteria: keyof User | SortingFunction<User>) => {
   } else {
     sortedData = [...formattedData].sort(filterCriteria);
   }
-  setFormattedData(sortedData);
 };
+
+ useEffect(()=> {
+  setFormattedData(formattedData)
+ }, [formattedData]);
+
+
 
   return { formatted: formattedData, search, filter, sortBy };
 };
